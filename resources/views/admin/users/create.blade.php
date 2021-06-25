56fb504e19174a78016f1874914be6e255851d4b
Debasis Chakraborty <chakraborty.debasis99@gmail.com>
2021-06-25 10:31:12 +0530

    Nug fix

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -16,20 +16,20 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="form-group">
-                    <label>First Name</label>
-                    <input type="text" class="form-control" name="first_name"/>
+                    <label>First Name<span class="text-danger" style="user-select: auto;">*</span></label>
+                    <input type="text" class="form-control" name="first_name" required/>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-group">
-                    <label>Last Name</label>
-                    <input type="text" class="form-control" name="last_name" />
+                    <label>Last Name<span class="text-danger" style="user-select: auto;">*</span></label>
+                    <input type="text" class="form-control" name="last_name" required/>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-group">
-                    <label>Email</label>
-                    <input type="text" class="form-control" name="email" />
+                    <label>Email<span class="text-danger" style="user-select: auto;">*</span></label>
+                    <input type="email" class="form-control" name="email" required/>
                 </div>
             </div>
             <div class="col-md-4">
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -17,20 +17,20 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="form-group">
-                    <label>First Name</label>
-                    <input type="text" class="form-control" value="{{$user->first_name}}" name="first_name"/>
+                    <label>First Name<span class="text-danger" style="user-select: auto;">*</span></label>
+                    <input type="text" class="form-control" value="{{$user->first_name}}" name="first_name" required/>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-group">
-                    <label>Last Name</label>
-                    <input type="text" class="form-control" value="{{$user->last_name}}" name="last_name" />
+                    <label>Last Name<span class="text-danger" style="user-select: auto;">*</span></label>
+                    <input type="text" class="form-control" value="{{$user->last_name}}" name="last_name" required/>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="form-group">
-                    <label>Email</label>
-                    <input type="text" class="form-control" value="{{$user->email}}" name="email" />
+                    <label>Email<span class="text-danger" style="user-select: auto;">*</span></label>
+                    <input type="text" class="form-control" value="{{$user->email}}" name="email" required/>
                 </div>
             </div>
             <div class="col-md-4">
